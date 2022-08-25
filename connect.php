64302e3e155c891ffabe119da739946a8763ac70
Yohan Launay <106663340+Yohan-Launay@users.noreply.github.com>
2022-08-25 18:59:30 +0200

    html/css end - created folder php

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+//connexion à la base distante
+define ("LOGIN", "root");
+define ("PASSE", "");
+define ("SERVEUR", "localhost");
+define ("PORT", "3306");
+define ("BASE", "formulairephp");
+
+
+?>
---
+++ function.php
@@ -0,0 +1,13 @@
+
+<?php
+
+function connexion()
+	{
+	require_once("connect.php");
+	//si numéro de port
+	//$connexion = mysqli_connect(SERVEUR,LOGIN,PASSE,BASE,PORT) or die("Error " . mysqli_error($connexion));
+	//si pas de numéro de port
+	$connexion = mysqli_connect(SERVEUR,LOGIN,PASSE,BASE) or die("Error " . mysqli_error($connexion));
+
+	return $connexion;
+}
